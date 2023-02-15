9f393836990ee6dbda5c84f6a21baeb35399e6b2
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-02-15 16:51:01 +0900

    삽입정렬까지
    삽입정렬까지 공부하고 코드 구현함.

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -11,9 +11,12 @@ public class Sorting {
         int k, tmp;
         for (int last = A.length - 1; last >= 1; last--) {
             k = theLargest(last); // A[0 ... last]중에서 가장 큰 수 A[k]를 찾는다.
-            tmp = A[k]; A[k] = A[last]; A[last] = tmp;
+            tmp = A[k];
+            A[k] = A[last];
+            A[last] = tmp;
         }
     }
+
     private int theLargest(int last) {
         // A[0 ... last]에서 가장 큰 수의 인덱스를 리턴한다.
         int largest = 0;
@@ -22,6 +25,7 @@ public class Sorting {
         return largest;
     }
 
+    // 버블 정렬
     public void bubbleSort() {
         int tmp = 0;
         boolean swapped;
@@ -29,7 +33,7 @@ public class Sorting {
             swapped = false;
             for (int i = 0; i <= last - 1; i++)
                 if (A[i] > A[i + 1]) {
-                    tmp = A[i]
+                    tmp = A[i];
                     A[i] = A[i + 1];
                     A[i + 1] = tmp;
                     swapped = true;
@@ -39,4 +43,17 @@ public class Sorting {
         // tmp = tmp; // 이 코드는 무슨 의미인 것일까?
     }
 
+    // 삽입 정렬
+    public void insertionSort() {
+        for (int i = 1; i < A.length - 1; i++) {
+            int loc = i - 1;
+            int newItem = A[i];
+            while (loc >= 0 && newItem < A[loc]) {
+                A[loc + 1] = A[loc];
+                loc--;
+            }
+            A[loc + 1] = newItem;
+        }
+    }
+    
 }
